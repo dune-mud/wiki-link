2374dc8d30d340d5e80ace2710dea81536b5aa80
Daniel McCarney <daniel@binaryparadox.net>
2023-06-24 15:15:44 -0400

    wiki-link: arg parse and logging

--- wiki-link.py
+++ wiki-link.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
+import logging
 
 
-def usage() -> None:
-    print(f"{sys.argv[0]} <wiki src dir> <markdown dest dir>")
+def log_init(level: str) -> None:
+    numeric_level = getattr(logging, level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"invalid log level: {level}")
+    logging.basicConfig(level=numeric_level)
 
 
-def main(wiki_src: str, markdown_dest: str) -> None:
-    print(f"wiki-sync'ing from {wiki_src} to {markdown_dest}")
+def main(*, wiki_src: str, markdown_dest: str) -> None:
+    logging.info(f"wiki-sync'ing from {wiki_src} to {markdown_dest}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        usage()
-        exit(1)
+    parser = argparse.ArgumentParser(
+        prog="wiki-link",
+        description="Renders DokuWiki from one place, to markdown in another",
+    )
+    parser.add_argument("wiki_src_dir")
+    parser.add_argument("markdown_dest_dir")
+    parser.add_argument("-l", "--log", default="info")
+    args = parser.parse_args()
 
-    main(sys.argv[1], sys.argv[2])
+    log_init(args.log)
+    main(wiki_src=args.wiki_src_dir, markdown_dest=args.markdown_dest_dir)
